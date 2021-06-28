b09e6212afe330dd176748453e8995bf97d3a15c
lexd283 <80298061+lexd283@users.noreply.github.com>
2021-06-28 11:47:27 -0400

    Update lexd_metar.py

--- lexd_metar.py
+++ lexd_metar.py
@@ -22,14 +22,14 @@ load_dotenv()
 secret = os.getenv("API_KEY")
 
 async def main():
-    icao = input('ICAO or IATA: ')
+    lexd = input('ICAO or IATA: ')
     os.system('cls')
-    headers = {'Authorization': f'{secret}'}
-    ok = requests.get(f'https://avwx.rest/api/metar/{icao}?airport=true&reporting=true&format=json', headers=headers)
+    headers = {'Authorization': secret}
+    ok = requests.get(f'https://avwx.rest/api/metar/{lexd}?airport=true&reporting=true&format=json', headers=headers)
     joe = ok.json()
     if "error" in joe:
-        return print('sorry there is an error, rerun the code and try another airport')
-    print(f'lexd_metar.py\nSTATION: {icao.upper()}\nRAW REPORT: {joe["raw"]}\nTIME OF OBSERVATION: {joe["time"]["repr"]} ({joe["time"]["dt"]})\nWIND: {joe["wind_direction"]["value"]}°@{joe["wind_speed"]["value"]}{joe["units"]["wind_speed"]}\nDEWPOINT: {joe["dewpoint"]["value"]}\nTEMPERATURE: {joe["temperature"]["value"]}')
+        return print(f'{lexd} is not supported, or there was an error fetching information.')
+    print(f'lexd_metar.py\nSTATION: {lexd.upper()}\nRAW REPORT: {joe["raw"]}\nTIME OF OBSERVATION: {joe["time"]["repr"]} ({joe["time"]["dt"]})\nWIND: {joe["wind_direction"]["value"]}°@{joe["wind_speed"]["value"]}{joe["units"]["wind_speed"]}\nDEWPOINT: {joe["dewpoint"]["value"]}\nTEMPERATURE: {joe["temperature"]["value"]}')
     results = [x['repr'] for x in joe['clouds']]
     x = 0
     new_results = []
